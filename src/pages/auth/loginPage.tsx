91e0f7d6e8a02186912a70b35b5ada16b1ac4ffe
EngHikmat <hekmatmalkawi@hotmail.com>
2024-11-26 01:16:32 +0300

    add failed login toast

--- src/pages/auth/loginPage.tsx
+++ src/pages/auth/loginPage.tsx
@@ -6,6 +6,7 @@ import { LoginSchema } from "lib/schemas/login.schema";
 import { LoginType } from "lib/types/login.types";
 import FormField from "components/form-field";
 import useAuthStore from "store/auth.store";
+import { Toaster, toaster } from "components/ui/toaster";
 
 const LoginPage = () => {
   const auth = useAuthStore();
@@ -20,10 +21,19 @@ const LoginPage = () => {
   const onSubmit = (data: LoginType) => {
     if (data.username === "user" && data.password === "password") {
       auth.login(data);
+    } else {
+      toaster.create({
+        title: "Ops!",
+        description:
+          "Your login attempt was unsuccessful. Please double-check your credentials and try again.",
+        type: "error",
+        duration: 3000,
+      });
     }
   };
   return (
     <Box h="100vh">
+      <Toaster />
       <HStack h="100%" align="stretch" gap="0">
         <IntroSide />
         <Box flex="50%" p="5">
