29e082ec887841ef85ceec731efd1fbb4ce9b1ec
EngHikmat <hekmatmalkawi@hotmail.com>
2024-11-25 23:29:27 +0300

    update

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,29 @@
-import ErrorMessagePage from "pages/errorMessagePage";
-import LoginPage from "pages/loginPage";
-import SalesDataPage from "pages/salesDataPage";
-import UserMetricsPage from "pages/userMetricsPage";
+import ErrorMessagePage from "pages/home/errorMessagePage";
+import Layout from "pages/home/layout";
+import LoginPage from "pages/auth/loginPage";
+import SalesDataPage from "pages/home/salesDataPage";
+import UserMetricsPage from "pages/home/userMetricsPage";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <UserMetricsPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <UserMetricsPage />,
+      },
+      {
+        path: "/sales-data",
+        element: <SalesDataPage />,
+      },
+      {
+        path: "/error-ui",
+        element: <ErrorMessagePage />,
+      },
+    ],
   },
-  {
-    path: "/sales-data",
-    element: <SalesDataPage />,
-  },
-  {
-    path: "/error-ui",
-    element: <ErrorMessagePage />,
-  },
+
   {
     path: "/login",
     element: <LoginPage />,
