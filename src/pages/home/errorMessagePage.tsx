28d028d4c0bc8c7af7c3819a892cefcf31d15b23
EngHikmat <hekmatmalkawi@hotmail.com>
2024-11-26 11:22:06 +0300

    Invalid Endpoint page represent

--- src/pages/home/errorMessagePage.tsx
+++ src/pages/home/errorMessagePage.tsx
@@ -1,5 +1,20 @@
+import Loader from "components/loader";
+import Rbox from "components/r-box";
+import { Alert } from "components/ui/alert";
+import { useGetInvalidEndpoint } from "services/hooks/InvalidEndPoint";
+
 const ErrorMessagePage = () => {
-  return <div>ErrorMessagePage</div>;
+  const { isLoading, isError, error } = useGetInvalidEndpoint();
+  return (
+    <Rbox title="Error Message Page" isBorder>
+      {isLoading && <Loader />}
+      {isError && (
+        <Alert status="error" title={error.name}>
+          {error.message}
+        </Alert>
+      )}
+    </Rbox>
+  );
 };
 
 export default ErrorMessagePage;
